85cab2b7ce84c54f1b192ce4cc99a9858496269b
Jesus <jesusmueguz177@gmail.com>
2026-07-31 10:32:58 -0600

    commit de dashboard

---
+++ src/components/DashBoard.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View,Button,Alert,TextInput,TouchableOpacity,Image} from 'react-native';
+import { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+
+export default function Inicio(){
+return (
+   <View style={styles.container}>
+
+</View>
+        )
+
+
+}
+
+const styles = StyleSheet.create({
+  container: { 
+    flex: 1,
+    backgroundColor: '#ecefb9',
+    alignItems: 'center',
+    justifyContent: 'center',  }
+});
+
+
