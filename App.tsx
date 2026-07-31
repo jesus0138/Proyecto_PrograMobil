ac6261611d24c9c1bbcdcfc01565d57817522c53
Jesus <jesusmueguz177@gmail.com>
2026-07-31 12:50:25 -0600

    commit de dashboard

--- App.tsx
+++ App.tsx
@@ -2,12 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View,Button,Alert,TextInput} from 'react-native';
 import { useState } from 'react';
 import Inicio from './src/components/inicio';
-
+import DashBoard from './src/components/DashBoard';
 export default function App() {
   
   return (
     <View style={styles.container }>
-      <Inicio></Inicio>
+      <DashBoard></DashBoard>
     </View>
   );
 }
--- src/components/DashBoard.tsx
+++ src/components/DashBoard.tsx
@@ -5,13 +5,48 @@ import { StatusBar } from 'expo-status-bar';
 
 export default function Inicio(){
 return (
-   <View style={styles.container}>
-<TuchableOpacity style={styles.boton} onPress={() => Alert.alert('Botón presionado')}>
 
-            </TouchableOpacity>
-            <StatusBar style="auto" />
-</View>
-        )
+
+        <View style={styles.container2}>
+           <Text style={styles.Titulo}>Inventario</Text> 
+
+                     <TouchableOpacity style={styles.boton} >
+<Text style={styles.boton}>Asignaciones</Text >
+<Image source={require('./icon_asig.png')}style={{ width: 20, height: 20, marginLeft: 10 }}/>
+                     </TouchableOpacity>
+
+                     <TouchableOpacity style={styles.boton} >
+<Text style={styles.boton}>Inventario</Text>
+<Image source={require('./icon_asig.png')}style={{ width: 20, height: 20, marginLeft: 10 }}/>
+                     </TouchableOpacity>
+
+                     <TouchableOpacity style={styles.boton} >
+<Text style={styles.boton}>herramientas</Text>
+<Image source={require('./icon_asig.png')}style={{ width: 20, height: 20, marginLeft: 10 }}/>
+                     </TouchableOpacity>
+ 
+ <TouchableOpacity style={styles.boton} >
+<Text style={styles.boton}>Entradas</Text>
+<Image source={require('./icon_asig.png')}style={{ width: 20, height: 20, marginLeft: 10 }}/>
+                     </TouchableOpacity>
+                     
+                     <TouchableOpacity style={styles.boton} >
+<Text style={styles.boton}>Salidas</Text>
+<Image source={require('./icon_asig.png')}style={{ width: 20, height: 20, marginLeft: 10 }}/>
+                     </TouchableOpacity>
+
+<TouchableOpacity style={styles.boton} >
+<Text style={styles.boton}>Reportes</Text>
+<Image source={require('./icon_asig.png')}style={{ width: 20, height: 20, marginLeft: 10 }}/>
+                     </TouchableOpacity>
+
+
+                          <StatusBar style="auto" />
+
+   </View>
+    
+   
+    )
 
 
 }
@@ -21,12 +56,36 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#ecefb9',
     alignItems: 'center',
-    justifyContent: 'center',  }
+    justifyContent: 'center',  
+   
+},
+container2: { 
+    flex: 1,
+    backgroundColor: '#ecefb9',
+    alignItems: 'center',
+    justifyContent: 'center', 
+    flexDirection: 'column', 
+   
+},
 
 
+    Titulo: {
+        fontSize: 24,
+        color: 'pink',
+        textAlign: 'center',
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
     boton: {
         backgroundColor: '#4CAF50',
-        padding: 10,    }
+        padding: 10,  
+     flexDirection: 'row',
+     textAlign: 'center',
+        borderRadius: 5,
+        marginBottom: 10,
+        alignItems: 'center',
+    }
+    
 });
 
 
